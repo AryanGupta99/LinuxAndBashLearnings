ddf414cf0435edc1ba224f706ecda73f1b786de0
AryanGupta99 <aryangupta7825@gmail.com>
2022-10-14 19:33:14 +0530

    first shell script modified a little bit with if else

--- Shell-Scripting/first_shell_script.sh
+++ Shell-Scripting/first_shell_script.sh
@@ -6,3 +6,16 @@ echo "But i Must Work hard for it"
 sleep 4s 
 echo "And Also i will have to learn the core values and core principals of Devops"
 
+name="Aryan Gupta"
+echo "Hello ${name},Please enter your age "
+
+read age
+
+echo "Hello ${name}, You are ${age} years old"
+
+if [ "$1" = "aryan" ]
+then
+ echo "you are aryan"
+else
+ echo "you are somebody else"
+fi
