12b8d4f33142daeca0c269df8262fff6fe76ceb3
AryanGupta99 <aryangupta7825@gmail.com>
2022-10-14 21:20:49 +0530

    More shell scripts added by Aryan

--- Shell-Scripting/shell_scripts.sh
+++ Shell-Scripting/shell_scripts.sh
@@ -50,7 +50,90 @@ tar czf $backup_file $src_dir
 
 echo "Backup Complete"
 
+#while loop
 
+valid=true
+count=1
+while [ $valid ]
+do
+echo $count
+if [ $count -eq 5 ];
+then
+break
+fi
+((count++))
+done
+
+#if else statements
+
+echo "Enter username"
+read username
+echo "Enter password"
+read password
+
+if [[ ( $username == "admin" && $password == "secret" ) ]]; then
+echo "valid user"
+else
+echo "invalid user"
+fi
+
+#switch statements
+
+echo "Enter your lucky number"
+read n
+case $n in
+101)
+echo echo "You got 1st prize" ;;
+510)
+echo "You got 2nd prize" ;;
+999)
+echo "You got 3rd prize" ;;
+*)
+echo "Sorry, try for the next time" ;;
+esac
+
+#arguments 
+echo "Total arguments : $#"
+echo "1st Argument = $1"
+echo "2nd argument = $2"
+
+#arguments input using loops and stuff
+
+for arg in "$@"
+do
+index=$(echo $arg | cut -f1 -d=)
+val=$(echo $arg | cut -f2 -d=)
+case $index in
+X) x=$val;;
+
+Y) y=$val;;
+
+*)
+esac
+done
+((result=x+y))
+echo "X+Y=$result"
+
+#directory making
+
+echo "Enter directory name"
+read ndir
+if [ -d "$ndir" ]
+then
+echo "Directory exist"
+else
+`mkdir $ndir`
+echo "Directory created"
+fi
+
+#check if file exist 
+
+filename=$1
+if [ -f "$filename" ]; then
+echo "File exists"
+else
+echo "File does not exist"
+fi
 
 
 #MAIN
