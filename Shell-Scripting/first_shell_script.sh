115f392a0fa8cbc1757a2c0700542291e3204143
AryanGupta99 <aryangupta7825@gmail.com>
2022-10-14 19:03:17 +0530

    my first bash script

---
+++ Shell-Scripting/first_shell_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "i will be a great DevOps Engineer"
+sleep 3s
+echo "But i Must Work hard for it"
+sleep 4s 
+echo "And Also i will have to learn the core values and core principals of Devops"
+
